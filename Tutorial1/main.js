935b7d4d5e79f46b26b72d3857ed6e2595075860
b2ankit <ankit15ec007@gmail.com>
2021-05-24 09:44:17 +0530

    first commit

---
+++ Tutorial1/main.js
@@ -0,0 +1,7 @@
+console.log("Hello World");
+const a = 10;
+const b = 20;
+console.log("Sum of a(="+a,")and b(="+b,") is : "+(a+b));
+console.log("Sub of a(="+a,")and b(="+b,") is : "+(a-b));
+console.log("Mul of a(="+a,")and b(="+b,") is : "+(a*b));
+
---
+++ Tutorial2/main.js
@@ -0,0 +1,36 @@
+// Type of function in js. Below are the Details:
+// 1.Basic Function
+// 2.Function Expresion
+// 3.Arrow Function
+// 4.Function Shorthand method
+// 5.Geneartor Function
+
+
+
+// Example of basic Function
+
+function sum_of_num(a, b){
+    console.log("[Basic function] sum of number is : "+(a+b));
+}
+
+// Example of Function Expression
+var sum_of_num1 = function(a,b){
+    console.log("[function Expression] sum of number is : "+(a+b));
+}
+
+// Example of Arrow function
+
+var sum_of_num2 = (a,b) =>{
+    console.log("[Arrow Function] sum of number is : "+(a+b));
+}
+
+// Example of Function Shorthand method
+
+// Example of Geneartor Function
+
+
+
+
+sum_of_num(10,10)
+sum_of_num1(20,20)
+sum_of_num2(30,30)
